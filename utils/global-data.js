e2dba8da594d596e0bb2ce5a2b277a4b89a5f5e7
Kane Rogers <kane.m.rogers@gmail.com>
2023-07-26 13:11:35 +1000

    Craft excellent content

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Adequate Games 👍';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : "We make games that are, you know, alright. They're okay.";
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '';
 
   return {
     name,
